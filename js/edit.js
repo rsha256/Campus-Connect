ef98f9c09197bc6480699b60d28fbec22f042c33
DefCon42 <ok@kekmai.top>
2018-04-28 18:58:39 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ js/edit.js
@@ -0,0 +1,51 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$("#submit").click(() => {
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "api/user/updateUser", true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify({
+		username: getParameterByName('user'),
+		name: $("#user").val(),
+		campus: $("#campus").val(),
+		email: $("#email").val(),
+		tags: $("#tags").val()
+	}));
+	xhr.onload = function() {
+		alert(this.responseText);
+	}	
+});
+
+$("#tags").on('input', () => {
+	if ($("#tags").val().includes(' '))
+		$("#tags").val($("#tags").data('in'));
+		
+	$("#tags").data('in', $("#tags").val());
+});
+
+$("#back").click(() => {
+	$(location).attr('href',"http://very.hardcoded.software/profile.html?user=" + getParameterByName('user'));	
+});
+
+$("#logout").click(() => {
+	$(location).attr('href',"http://very.hardcoded.software/");	
+});
+
+$(document).ready(() => {
+	var url = "api/user/get?user=" + getParameterByName('user');
+	$.getJSON(url, function(data) {	
+		console.log(data);
+		$("#user").val(data.name);
+		$("#campus").val(data.campus);
+		$("#email").val(data.email);
+		$("#tags").val(data.tags);
+    });
+});
